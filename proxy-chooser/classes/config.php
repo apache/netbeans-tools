a31d317685f632d29b9113d749aae04bb055b073
Matthias Bläsing <mblaesing@doppel-helix.eu>
2021-03-02 19:51:42 +0100

    Fix mirror URL generator
    - fix path to original updates.xml.gz files
    - add netbeans.hash.code to URL

--- proxy-chooser/classes/config.php
+++ proxy-chooser/classes/config.php
@@ -7,11 +7,11 @@ $CONFIG = array(
     "mirrorCacheFile" => __DIR__ . "/../data/mirrors.list",
     // Path to the updates.xml - will be passed through sprintf, first parameter
     // is the netbeans version. It is expected, that updates.xml
-    "updatesPattern" => __DIR__ . '/../../uc/%1$s/updates.xml.gz',
+    "updatesPattern" => '/var/www/html/uc/%1$s/updates.xml.gz',
     // Location where the generated updates files are cached (first parameter
     // is netbeans version, second parameter is the chosen proxy)
     "cachePattern" => __DIR__ . '/../data/%1$s/%2$s/updates.xml.gz',
     // Pattern for Update Center URL, parameter 1 is the request host,
     // parameter 2 is the chosen mirror
-    "updateCenterUrl" => 'https://%1$s/uc-proxy-chooser/<strong>version</strong>/updates.xml.gz?mirror=%2$s'
+    "updateCenterUrl" => 'https://%1$s/uc-proxy-chooser/<strong>version</strong>/updates.xml.gz?{$netbeans.hash.code}&mirror=%2$s'
 );
